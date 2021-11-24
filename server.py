4ba397728014c675a20a58315cab091c0fd1a6d2
EmilianOnofras <67412680+EmilianOnofras@users.noreply.github.com>
2021-11-24 15:40:14 +0200

    changed header

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<html><head><title>Definetly a website</title></head>", "utf-8"))
         self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("<body>", "utf-8"))
         self.wfile.write(bytes("<p>This server shall do servery things and look very impressive while doing so.</p>", "utf-8"))
